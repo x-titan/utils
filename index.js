da341eded4980ee52c373a55dd4ed272af70a2ca
Aset Telmanov <50516342+x-titan@users.noreply.github.com>
2021-07-03 14:17:41 +0500

    remove xcore

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-import XCore from "./core/xcore.js"
 import is from "./core/types.js"
 import each from "./core/each.js"
-import { Extend, Mixin, Mono } from "./core/object.js"
+import * as XObject from "./core/object.js"
 
-XCore.define("is", is)
-XCore.define("each", each)
-XCore.define("Extend", Extend)
-XCore.define("Mixin", Mixin)
-XCore.define("Mono", Mono)
-
-export default XCore
+export { is, each, XObject }
