bb285e10c063adf532efa10a92128843128550f4
Aset Telmanov <telmanov2002.at@gmail.com>
2021-09-20 21:12:23 +0500

    update and fix extend

--- core/object/extend.js
+++ core/object/extend.js
@@ -7,11 +7,17 @@ import is from "../types.js"
  * @return {obj & source}
  */
 export default function extend(obj, ...source) {
-  if (is.obj(obj))
-    each(source, s => {
-      if (is.obj(s)) each.obj(s, (z, k) =>
-        obj[k] = is.func(z) ? z.bind(obj) : z
-      )
-    }, false)
+  if (is.obj(obj)) each(source, s => {
+    if (is.obj(s)) each.obj(s, (z, k) => obj[k] = z)
+  }, false)
+  return obj
+}
+
+extend.pro = (obj, ...source) => {
+  if (is.obj(obj)) each(source, s => {
+    if (is.obj(s)) each.obj(s, (z, k) =>
+      obj[k] = is.func(z) ? z.bind(obj) : z
+    )
+  }, false)
   return obj
 }
