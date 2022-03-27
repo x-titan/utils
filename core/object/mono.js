4edb36e5c39451a866c8faa55aa0f3bb785fb9cd
x-titan <50516342+x-titan@users.noreply.github.com>
2022-03-27 22:24:32 +0500

    ok

--- core/object/mono.js
+++ core/object/mono.js
@@ -1,7 +1,9 @@
 import is from "../types.js"
 
-const _listMono = new Set(["Mono"]), onerror_ = () => {
-  throw new Error("Objects of the Mono class must be in only one instance")
+const makeError = name => {
+  throw new Error(
+    "Objects of the Mono class must be in only one instance. " +
+    "This class " + name + " has already been used")
 }
 /**
  * Сhecks whether this item is in the list and returns the result.
@@ -11,14 +13,16 @@ const _listMono = new Set(["Mono"]), onerror_ = () => {
  *
  * Else `true`. This means that the item is present in the list.
  */
-export default class Mono {
+class Mono {
   /** @param {Function} onerror Calling on error */
-  constructor(onerror) { Mono.force(this, onerror) }
-  /**
-   * @param {Object} self
-   * @param {*} [self.]
-   */
-  static has(self) { _listMono.has(self.constructor.name) }
+  constructor(onerror) {
+    const target = new.target
+    if (constructorList.has(target))
+      return is.func(onerror) ? onerror() : makeError(target.name)
+    constructorList.add(target)
+  }
+  /** @param {new unknown} target */
+  static has(target) { return constructorList.has(target) }
   /**
    * Сhecks whether this item is in the list and returns the result.
    * After checking, it is added to the list.
@@ -26,14 +30,44 @@ export default class Mono {
    * If the item is in the list returns `false`. So it's okay.
    *
    * Else `true`. This means that the item is present in the list.
-   * @param {Object} self `this`
-   * @param {*} [self.]
+   * @param {Object} target `this`
+   * @param {*} [target.]
    * @param {Function} onerror Calling on error
    */
-  static force(self, onerror) {
-    if (!is.obj(self)) throw new TypeError("Bad Object")
-    if (!is.func(onerror)) onerror = onerror_
-    if (_listMono.has(self = self.constructor.name)) onerror()
-    else _listMono.add(self)
+  static mixin(target, onerror) {
+    if (typeof target !== "object")
+      throw new Error("Bad argument. Required object")
+    const cons = target.constructor
+    if (this.has(cons))
+      return is.func(onerror) ? onerror() : makeError(cons.name)
+    constructorList.add(cons)
+    return target
+  }
+  /**
+   * @param {new unknown} target
+   * @param {Function} onerror
+   * @return {target}
+   */
+  static mono(target, onerror) {
+    if (typeof target !== "function" ||
+      typeof target.constructor !== "function")
+      throw new Error("Bad argument. Required class or function")
+    const _ = function (...args) {
+      return Mono.mixin(new target(...args), onerror)
+    }
+    try {
+      if (!target.prototype)
+        Object.setPrototypeOf(target.prototype = {})
+      target.constructor = target.prototype.constructor = _
+      _.prototype = target.prototype || {}
+    } catch (e) {
+      console.warn("Mono error. class " +
+        target.name + " not be a mixin to Mono")
+      console.trace(e)
+    }
+    return _.constructor = _.prototype.constructor = _
   }
 }
+const constructorList = new Set([Mono])
+
+export default Mono
--- core/types.js
+++ core/types.js
@@ -1,20 +1,22 @@
 const { freeze, assign } = Object
-const { toString } = Object.prototype
+const { toString } = Reflect
 const { isInteger, isSafeInteger } = Number
 const { iterator, toStringTag } = Symbol
-const objCons = {}.constructor
-const isOfType = type => value => typeof value === type
-const getObjName = value => toString.call(value).slice(8, -1) || null
-const is = value => {
-  if (value === null) return "null"
-  switch (getObjName(value)) {
-    case "Array": return "array"
-    case "Map": return "map"
-    case "Set": return "set"
-    case "Error": return "error"
-  }
-  return typeof value
+const objCtor = {}.constructor
+
+/** @return {string} */
+function getCtorName(value) {
+  return typeof value.constructor === "function" ?
+    value.constructor.name : null;
 }
+function isOfType(type) {
+  return value => typeof value === type;
+}
+/** @return {string} */
+function getObjName(value) {
+  return toString.call(value).slice(8, -1) || null;
+}
+
 const _ = {
   empty: value => value === undefined || value === null,
   null: value => value === null,
@@ -22,9 +24,15 @@ const _ = {
   nonZeroValue: value => !(_.empty(value) || value === 0 ||
     ((_.str(value) || _.arrayLike(value)) && value.length === 0)),
 
-  num: value => isFinite(value),
+  num: isFinite,
   str: isOfType("string"),
   func: isOfType("function"),
+  genFunc: value => _.func(value) &&
+    getCtorName(value) === "GeneratorFunction",
+  asyncFunc: value => _.func(value) &&
+    getCtorName(value) === "AsyncFunction",
+  anonymFunc: value => _.func(value) &&
+    value.prototype?.constructor !== value,
   symbol: isOfType('symbol'),
   bool: value => value === !!value,
   obj: value => !_.empty(value) && typeof value === "object",
@@ -32,9 +40,9 @@ const _ = {
   class: value => _.func(value) && ("" + value).startsWith('class '),
   notClass: value => _.empty(value) || value === globalThis,
   plainObj: value => _.obj(value) && getObjName(value) === "Object" &&
-    (value = value.constructor, value === null || value === objCons),
+    (value = value.constructor, value === null || value === objCtor),
   error: value => value instanceof Error,
-  argument: value => _.arrayLike(value) &&
+  args: value => _.arrayLike(value) &&
     getObjName(value) === "Arguments",
 
   int: isInteger,
@@ -51,14 +59,41 @@ const _ = {
   arrayLike: value => _.obj(value) &&
     _.int(value.length) && value.length > -1,
   iterable: value => !_.empty(value) && _.func(value[iterator]),
+  buffer: value => {
+    if (value.constructor && _.func(value.constructor.isBuffer))
+      return value.constructor.isBuffer(value)
+    return false
+  }
+}
+function is(value) {
+  if (value === undefined) return "undefined"
+  if (value === null) return "null"
+
+  if (_.array(value)) return "array"
+  if (_.buffer(value)) return "buffer"
+  if (_.error(value)) return "error"
+  if (_.args(value)) return "arguments"
+
+  const t = typeof value
+  if (t === "function") {
+    if (_.asyncFunc(value)) return "asyncfunction"
+    if (_.genFunc(value)) return "generatorfunction"
+    return t
+  }
+  return t
 }
 freeze(assign(is, _, {
   null_: _.null,
   undefined_: _.undefined,
   class_: _.class,
   NaN: _.nan,
+  argument: _.args,
+  arguments_: _.args,
   [toStringTag]: "is",
   toString,
-  constructor: null
+  getObjName,
+  getCtorName,
+  isOfType,
+  constructor: null,
 }))
 export default is
