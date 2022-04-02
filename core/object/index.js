507303781f66c92e6fe3d4b34945001dbe30ef79
x-titan <50516342+x-titan@users.noreply.github.com>
2022-04-02 22:24:59 +0500

    import null.js

--- core/object/index.js
+++ core/object/index.js
@@ -2,5 +2,6 @@ import binder from "./binder.js"
 import extend from "./extend.js"
 import mixin from "./mixin.js"
 import Mono from "./mono.js"
+import Null from "./null.js"
 
-export { binder, extend, mixin, Mono }
+export { binder, extend, mixin, Mono, Null }
